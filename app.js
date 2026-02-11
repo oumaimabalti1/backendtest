8ce77ebc3e2271f8f771b01803ade3d0b2451698
oumaimabalti1 <baltioumaima1@gmail.com>
2026-02-11 12:56:13 +0100

    config server

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const http = require('http');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -38,4 +35,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+const server = http.createServer(app);
+server.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
+});
