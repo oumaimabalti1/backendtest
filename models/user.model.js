ffe38e3ef2c98a764f0084cfbabb29918669e88a
oumaimabalti1 <baltioumaima1@gmail.com>
2026-02-13 16:32:49 +0100

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose'); 
-
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true  },
-    email: {type: String, required: {true,  "Email is required"}, unique: true },
+    email: {type: String, required: [true, "Email is required"], unique: true },
     password: {type: String, required: true },
     role : {type: String, enum: ['admin', 'user'], default: 'user' }
 },
@@ -12,4 +12,16 @@ const userSchema = new mongoose.Schema({
 {timestamps: true}); //ytalla createdat w updatedat
 const User = mongoose.model('User', userSchema);
 
+
+userSchema.pre('save', async function(next) {
+    try {
+        if (!this.isModified('password')) return next();
+        const salt = await bcrypt.genSalt();
+        const user = this;
+        user.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 module.exports = User;
