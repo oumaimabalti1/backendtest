99839c4f3926ec7db5f74b0691bddf6b731fb117
oumaimabalti1 <baltioumaima1@gmail.com>
2026-03-11 11:53:41 +0100

    +

--- middlewares/uploadfile.js
+++ middlewares/uploadfile.js
@@ -1,28 +1,35 @@
 const multer = require("multer");
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
+
+const uploadPath = path.join(__dirname, '..', 'public', 'images');
+
+// Créer le dossier s'il n'existe pas
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
+
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/images')
-  },
-  filename: function (req, file, cb) {
-    const uploadPath = 'public/images';
-    const originalName = file.originalname;
-    console.log(file.originalname)
-    const fileExtension = path.extname(originalName);
-    let fileName = originalName;
-
-    // Vérifier si le fichier existe déjà
-    let fileIndex = 1;
-    while (fs.existsSync(path.join(uploadPath, fileName))) {
-      const baseName = path.basename(originalName, fileExtension);
-      fileName = `${baseName}_${fileIndex}${fileExtension}`;
-      fileIndex++;
+    destination: function (req, file, cb) {
+        cb(null, uploadPath);
+    },
+    filename: function (req, file, cb) {
+        // Nettoyer le nom du fichier (supprimer espaces et caractères spéciaux)
+        const ext = path.extname(file.originalname);
+        const baseName = path.basename(file.originalname, ext)
+            .replace(/\s+/g, '_')
+            .replace(/[^a-zA-Z0-9_-]/g, '');
+        
+        let fileName = `${baseName}${ext}`;
+        let fileIndex = 1;
+        while (fs.existsSync(path.join(uploadPath, fileName))) {
+            fileName = `${baseName}_${fileIndex}${ext}`;
+            fileIndex++;
+        }
+        console.log('Saving file:', fileName);
+        cb(null, fileName);
     }
-
-    cb(null, fileName);
-  }
-})
+});
 
 var uploadfile = multer({ storage: storage });
 module.exports = uploadfile;
